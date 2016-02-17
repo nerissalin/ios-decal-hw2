80c8308d55ca749c1ecff6e9f2e7af1ee3f66b3a
Nerissa Lin <lin.nerissa@berkeley.edu>
2016-02-16 19:42:59 -0800

    dopeswag

---
+++ Hw2 Keyboard/Hw2 Keyboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hw2 Keyboard
+//
+//  Created by Everest Shi on 2/16/16.
+//  Copyright © 2016 Nerissa Lin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hw2 Keyboard/Hw2 Keyboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hw2 Keyboard
+//
+//  Created by Everest Shi on 2/16/16.
+//  Copyright © 2016 Nerissa Lin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
